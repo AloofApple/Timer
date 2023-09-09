6ab099ffa93d2cdd944a2b97edc88ff2df90e7f0
Tjeh Chou <t.chou@student.tue.nl>
2023-09-09 09:39:48 +0200

    first commit

--- src/components/Controller.js
+++ src/components/Controller.js
@@ -9,10 +9,43 @@ export default class Controller extends React.Component {
     };
   }
 
-  handleChange = (event) => {
+  reset() {
+    this.setState({
+      hours: 0,
+      minutes: 0,
+    });
+  }
+
+  handleChangeHours = (event) => {
     const {name, value} = event.target;
 
-    this.setState({[name]: parseInt(value)});
+    this.setState({[name]: Math.min(parseInt(value), 24)});
+    
+    if (this.state.hours === 24 | parseInt(value) === 24) {
+      this.setState({minutes: 0});
+    }
+  }
+
+  handleChangeMinutes = (event) => {
+    const {name, value} = event.target;
+
+    if (this.state.hours === 24) {
+      this.setState({[name]: Math.min(parseInt(value), 0)});
+    } else {
+      this.setState({[name]: Math.min(parseInt(value), 59)});
+    }
+  }
+
+  StartTimer() {
+    // Remove the input
+    document.getElementById("hours").value = "";
+    document.getElementById("minutes").value = "";
+
+    // Reset state
+    this.reset();
+
+    // Start the timer
+    this.props.starttimer(this.state.hours, this.state.minutes);
   }
 
   render() {
@@ -20,13 +53,39 @@ export default class Controller extends React.Component {
       <>
         <div className="container"> 
             <div className="squarecontainer">
-              <button className="square " onClick={
-                  () => this.props.stoptimer()}
+              <button 
+                className="square blue" 
+                onClick={
+                  () => this.props.stoptimer()
+                }
               ></button>
-              <input type="number" name="hours" className="square grey" onChange={this.handleChange}/>
-              <input type="number" name="minutes" className="square grey" onChange={this.handleChange}/>
-              <button className="square yellow" onClick={
-                  () => this.props.starttimer(this.state.hours, this.state.minutes)}
+              <input 
+                id="hours"
+                type="number" 
+                name="hours"
+                value={this.state.hours} 
+                className="square grey" 
+                onChange={this.handleChangeHours} 
+                placeholder="H"
+                min="0"
+                max="24"
+              />
+              <input 
+                id="minutes"
+                type="number" 
+                name="minutes" 
+                value={this.state.minutes} 
+                className="square grey" 
+                onChange={this.handleChangeMinutes} 
+                placeholder="m"
+                min="0"
+                max="59"
+              />
+              <button 
+                className="square yellow" 
+                onClick={
+                  () => this.StartTimer()
+                }
               ></button>
             </div>
         </div>
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Controller from "./Controller";
+import Sound from "../Audio/Polaris.mp3";
 
 export default class Timer extends React.Component {
   constructor(props) {
@@ -8,100 +9,178 @@ export default class Timer extends React.Component {
       hours: 0,
       minutes: 0,
       seconds: 0,
+      running: "false",
     };
     this.StartTimer = this.StartTimer.bind(this);
     this.StopTimer = this.StopTimer.bind(this);
+    this.Audio = new Audio(Sound);
   }
 
   StartTimer(hours, minutes){
     // Initialize timer
-    if (hours !== 0 | minutes !== 0) {
+    if (this.state.running !== "paused") {
+      if (isNaN(hours)) {
+        hours = 0;
+      }
+      if (isNaN(minutes)) {
+        minutes = 0;
+      }
+      if (this.state.running) {
+        this.StopTimer();
+        this.setState({
+          running: "true",
+        });
+      }
+
       this.setState({
-        seconds: 60,
+        hours: hours,
+        minutes: minutes,
+        seconds: 0,
       });
-    } else {
-      return;
-    }
+    } 
 
     this.setState({
-      hours: hours, 
-      minutes: minutes
+      running: "true",
     });
+
+    document.body.style.backgroundColor = "#EBFF38";
+
+    // Make squares yellow
+    let Squares = this.SquareArray();
+    
+    for (let i = 0; i < Squares.length; i++) {
+      if (Squares[i].classList.contains("blue")) {
+        Squares[i].classList.remove("blue");
+        Squares[i].classList.add("yellow");
+      }
+    } 
 
     // Start timer
     this.timer = setInterval(() => {
       this.AnimateSeconds();
-      if (this.state.seconds === 0) {
-        if (this.state.minutes === 0) {
-          if (this.state.hours === 0) {
-            clearInterval(this.timer);
-          } 
-          else {
-            this.setState({
-              hours: this.state.hours - 1,
-              minutes: 59,
-            });
-          }
-        } 
-        else {
-          this.setState({
-            minutes: this.state.minutes - 1,
-            seconds: 59,
-          });
-        }
+
+      if (this.state.seconds > 0) {
+        this.setState(
+          {seconds: this.state.seconds - 1}
+        );
+      } 
+      else if (this.state.minutes > 0) {
+        this.setState({
+          minutes: this.state.minutes - 1,
+          seconds: 59,
+        });
+      } 
+      else if (this.state.hours > 0) {
+        this.setState({
+          hours: this.state.hours - 1,
+          minutes: 59,
+          seconds: 59,
+        });
       } 
       else {
+        this.StopTimer();
+        this.Audio.currentTime = 0;
+        this.Audio.play();
         this.setState({
-          seconds: this.state.seconds - 1,
+          running: "false",
         });
       }
     }, 1000);
   }
 
   StopTimer(){
+    document.body.style.backgroundColor = "#405BFF";
+
+    if (this.state.running === "false"){
+      return;
+    }
+
+    let Squares = this.SquareArray();
+
+    if (this.state.running === "paused"){
+      this.setState({
+        running: "false",
+      });
+
+      for (let i = 0; i < Squares.length; i++) {
+        if (Squares[i].classList.contains("blue")) {
+          Squares[i].classList.remove("blue");
+        }
+      } 
+
+      this.setState({
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+        running: "false",
+      });
+
+      return;
+    }
+
+    this.setState({
+      running: "paused",
+    });
+
+    for (let i = 0; i < Squares.length; i++) {
+      if (Squares[i].classList.contains("yellow")) {
+        Squares[i].classList.remove("yellow");
+        Squares[i].classList.add("blue");
+      }
+    }
+  
     clearInterval(this.timer);
+  }
+
+  SquareArray() {
+    return [
+      document.getElementById("square1"),
+      document.getElementById("square2"),
+      document.getElementById("square3"),
+      document.getElementById("square4"),
+    ];
   }
 
   FormatTime(time) {
     if (time < 10) {
       return "0" + time;
     }
+    else if (time === 60) {
+      return "00";
+    }
+
     return time;
   }
 
   AnimateSeconds() {
-    let squares = [
-      document.getElementById("square1"),
-      document.getElementById("square2"),
-      document.getElementById("square3"),
-      document.getElementById("square4"),
-    ]
-    let state = this.state.seconds % 5;
+    let SquareArraytate = this.state.seconds % 5;
+    let Squares = this.SquareArray();
 
-    switch(state) {
+    switch(SquareArraytate) {
       case 0:
-        squares[0].classList.remove("yellow");
-        squares[1].classList.remove("yellow");
-        squares[2].classList.remove("yellow");
-        squares[3].classList.remove("yellow");
+        Squares[0].classList.remove("yellow");
+        Squares[1].classList.remove("yellow");
+        Squares[2].classList.remove("yellow");
+        Squares[3].classList.remove("yellow");
         break;
       case 1:
-        squares[3].classList.add("yellow");
+        Squares[3].classList.add("yellow");
         break;
       case 2:
-        squares[2].classList.add("yellow");
+        Squares[2].classList.add("yellow");
         break;
       case 3:
-        squares[1].classList.add("yellow");
+        Squares[1].classList.add("yellow");
         break;
       case 4:
-        squares[0].classList.add("yellow");
+        Squares[0].classList.add("yellow");
         break;
       default:
         break;  
     }
   }
 
+  
 
   render() {
     return (
